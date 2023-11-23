c707bba0517acf9894f7cfb2b4f5b0f4c835c3f6
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2023-11-23 20:25:05 -0300

    feat: estrutura de deletar

---
+++ src/main/java/br/com/jpdev01/asaassdk/rest/action/Deleter.java
@@ -0,0 +1,36 @@
+package br.com.jpdev01.asaassdk.rest.action;
+
+import br.com.jpdev01.asaassdk.exception.ApiException;
+import br.com.jpdev01.asaassdk.http.Asaas;
+import br.com.jpdev01.asaassdk.http.AsaasRestClient;
+import br.com.jpdev01.asaassdk.http.Response;
+import br.com.jpdev01.asaassdk.utils.JsonUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpStatus;
+
+public abstract class Deleter<T> {
+
+    public T delete() {
+        return delete(Asaas.getRestClient());
+    }
+
+    public T delete(final AsaasRestClient client) {
+        Response response = client.post(getResourceUrl(), JsonUtil.toJSON(this));
+        return parseResponse(client, response);
+    }
+
+    private T parseResponse(AsaasRestClient client, Response response) {
+        if (response.getStatusCode() == HttpStatus.SC_BAD_REQUEST) {
+            throw new ApiException(HttpStatus.SC_BAD_REQUEST, response.getContent());
+        }
+
+        try {
+            return client.getObjectMapper().readValue(response.getContent(), getResourceClass());
+        } catch (Exception e) {
+            throw new RuntimeException("Error parsing response", e);
+        }
+    }
+
+    public abstract String getResourceUrl();
+    public abstract Class<T> getResourceClass();
+}
